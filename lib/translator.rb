2884132e1fe8549cc36b0422fb951c71799ff4c0
Noah Eakin <noaheakin@gmail.com>
2020-08-10 23:04:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ require 'pry'
 def load_library (file_path)
   library = YAML.load_file(file_path)
   update_library = library.each_with_object({}) do |(key, value), in_progress|
-    if !library[key]
-      library[key] = {
+    if !in_progress[key]
+      in_progress[key] = {
         english: value[0],
         japanese: value[1]
       }
